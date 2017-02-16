d354fe823ba349a3ba7138ab5a14f8350468739e
WINDOWS-J6BOKVP\Sam Blakeman <sam.blakeman03@gmail.com>
2017-02-16 21:28:05 +0000

    Starting to move AI over to C++

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGrounds.h"
+#include "ChooseNextWaypoint.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI is in C++!"));
+
+	return EBTNodeResult::Succeeded;
+}
+
+
