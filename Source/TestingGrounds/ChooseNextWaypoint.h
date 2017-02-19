3f24c781a2244e151727391a25ccfe58f530adcd
WINDOWS-J6BOKVP\Sam Blakeman <sam.blakeman03@gmail.com>
2017-02-19 12:08:57 +0000

    Patrolling using a C++ component architecture

--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -12,7 +12,17 @@ UCLASS()
 class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
+
+public:
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 };
